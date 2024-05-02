2107e874247ebbb6fff7edbef363bfa0983e51af
jcaudill0256 <157061713+jcaudill0256@users.noreply.github.com>
2024-05-01 22:31:16 -0400

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+﻿/*
+     Student Name: Jonathan Caudill
+     File Name: script.js
+     Date: 05/01/2024
+*/
+
+//Global variables
+var answer = document.querySelector("#answer p");
+var heading = document.querySelector("#answer h2");
+
+//Function to display the first answer
+function ans1() {
+    heading.style.display = "block";
+    answer.textContent = "I specialize in creating websites and programs.";
+}
+//Function to display the second answer
+function ans2() {
+    heading.style.display = "block";
+    answer.textContent = "By how many pages you want.";
+}
+//Function to display the third answer
+function ans3() {
+    heading.style.display = "block";
+    answer.textContent = "For one that is not very big 4-6 weeks.";
+}
+//Function to display the fourth answer
+function ans4() {
+    heading.style.display = "block";
+    answer.textContent = "No, I do not at this time.";
+}
